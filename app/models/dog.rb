2c478fd63e1ff3a44850cb84a9f63ac6b71c6653
Noah A Klem <noahaklem@gmail.com>
2021-09-20 16:29:58 -0600

    commit tree 3323ce89029c54ad512cc408d87959d38a495ff8

---
+++ app/models/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+    attr_accessor :name, :breed, :age
+
+    @@dogs = []
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+        save
+    end
+
+    def save
+        @@dogs << self
+    end
+
+    def self.all
+        @@dogs
+    end
+
+end
